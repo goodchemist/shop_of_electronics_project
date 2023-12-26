3f89f2d80c24947e29157177b73596ccff4ca597
good_chemist <iolgasergeevna@yandex.ru>
2023-12-26 20:01:10 +0800

    test __init__ for 'Keyboard' class

---
+++ tests/test_keyboard.py
@@ -0,0 +1,17 @@
+import pytest
+from src.keyboard import Keyboard
+
+
+@pytest.fixture
+def keyboard():
+    return Keyboard('Dark Project KD87A', 9600, 5)
+
+
+def test_init(keyboard):
+    """
+    Проверяет работу метода __init__.
+    """
+    assert keyboard.name == 'Dark Project KD87A'
+    assert keyboard.price == 9600
+    assert keyboard.quantity == 5
+    assert keyboard.language == 'EN'
